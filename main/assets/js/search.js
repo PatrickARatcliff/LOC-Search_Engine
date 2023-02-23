ec6600bc6b826c8fdd28e13be4bdc9aec62912da
PatrickARatcliff <pratcliff5@gmail.com>
2023-02-22 19:08:19 -0700

    set local storage for term/format on submit, poulate results cards

--- main/assets/js/search.js
+++ main/assets/js/search.js
@@ -3,6 +3,7 @@
 const searchForm = document.getElementById("search-form");
 const resultsContainer = document.getElementById("results-container");
 const searchContainer = document.getElementById("previous-search-container");
+const resultText = document.getElementById("result-text");
 //declare variables
 const apiBase = `https://www.loc.gov/`;
 const jsonParameter = `fo=json`;
@@ -16,6 +17,27 @@ const apiRequestUrl = `${apiBase}${searchFormat}/?${jsonParameter}&q=${searchTer
 // console.log(searchTerm);
 console.log(apiRequestUrl);
 
+function populateResults(resultData) {
+
+    let resultCard = document.createElement('div');
+    resultCard.innerHTML = `<div class="card my-2">
+    <div >
+        <img src="${resultData.image_url[0]}" class="card-img-top" alt="..." style="">
+    </div>
+    <div class="card-body">
+      <h5 class="card-title text-dark">${resultData.title}</h5>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">${resultData.date}</li>
+      <li class="list-group-item">${resultData.subject}</li>
+    </ul>
+    <div class="card-body">
+      <a href="${resultData.url}" class="card-link"  target="_blank">${resultData.url}</a>
+    </div>
+  </div>`
+  resultsContainer.append(resultCard)
+};
+
 function apiPromise(url) {
     fetch(url)
         .then((response) => {
@@ -26,6 +48,18 @@ function apiPromise(url) {
         })
         .then((data) => {
             console.log(data.search.query);
+            console.log(data.results)
+            resultText.textContent = '';
+            resultText.textContent = `${searchTerm}, ${searchFormat}`;
+            if (!data.results.length) {
+                console.log('No results found!');
+                resultsContainer.innerHTML = `<h5>No results for ${searchTerm}, ${searchFormat}</h5>`;
+            } else {
+                resultsContainer.innerHTML = '';
+                for (let i = 0; i < data.results.length; i++) {
+                    populateResults(data.results[i])
+                }
+            }
         })
         .catch((error) => window.alert(error))
 };
@@ -45,6 +79,9 @@ function searchFromSubmitHandler(event) {
     console.log(searchFormat.value);
 
     const queryString = `./results.html?q=${searchTerm}&format=${searchFormat}`;
+
+    localStorage.setItem('searchTerm', searchTerm);
+    localStorage.setItem('serarchFormat', searchFormat);
 
     location.assign(queryString);
 };
